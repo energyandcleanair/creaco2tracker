3b3566e3dbb4b906e0638d876b2792747351be0b
Hubert Thieriot <hubert.thieriot@gmail.com>
2022-12-07 09:08:34 +0000

    -split by year

--- R/get_co2_daily.R
+++ R/get_co2_daily.R
@@ -477,8 +477,6 @@ get_entsog <- function() {
       read_csv(sprintf('https://api.russiafossiltracker.com/v0/overland?format=csv&date_from=%s-01-01&date_to=%s-12-31&commodity=natural_gas', x, x))}) %>%
     bind_rows()
   
-  read_csv("https://api.russiafossiltracker.com/v0/overland?format=csv&date_from=2016-01-01&commodity=natural_gas")
-  
   types <- c('distribution','consumption','storage_entry','storage_exit','crossborder','production')
   entsog <- seq(2016, lubridate::year(lubridate::today())) %>%
     pbapply::pblapply(function(x){Sys.sleep(2);
