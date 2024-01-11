a30e598259d91ffb2b0f769b9e6be6f5e7abe8df
hubert-crea <hubert@energyandcleanair.org>
2024-01-11 13:54:19 +0800

    -adding date_from

--- R/download.R
+++ R/download.R
@@ -1,10 +1,10 @@
-download_co2_daily <- function(){
-  readr::read_csv('https://api.energyandcleanair.org/co2/emission?date_from=2015-01-01&format=csv') %>%
+download_co2_daily <- function(date_from="2015-01-01"){
+  readr::read_csv(glue('https://api.energyandcleanair.org/co2/emission?date_from={date_from}&format=csv')) %>%
     select(region, date, fuel, sector, unit, frequency, value, version)
 }
 
 download_gas_demand <- function(region_id=NULL){
-  
+
   params <- list(
     fuel='fossil_gas',
     data_source='crea',
@@ -12,20 +12,20 @@ download_gas_demand <- function(region_id=NULL){
     format='csv',
     region_id=region_id
   )
-  
+
   # Remove null elements
   params <- purrr::compact(params)
-  
+
   # Create URL params
   url_params <- paste(names(params), params, sep = "=", collapse = "&")
-  
+
   readr::read_csv(sprintf('https://api.energyandcleanair.org/energy/demand?%s', url_params)) %>%
     select(region_id, date, fuel, sector, unit, frequency, value)
 }
 
 
 download_electricity <- function(region_id=NULL, data_source=NULL){
-  
+
   params <- list(
     aggregate_by='country,source,date',
     data_source=data_source,
@@ -33,14 +33,14 @@ download_electricity <- function(region_id=NULL, data_source=NULL){
     format='csv',
     country=paste(region_id, collapse=',')
   )
-  
+
   # Remove null elements
   params <- purrr::compact(params)
-  
+
   # Create URL params
   url_params <- paste(names(params), params, sep = "=", collapse = "&")
-  
-  
+
+
   readr::read_csv(sprintf('https://api.energyandcleanair.org/power/generation?%s', url_params)) %>%
     rename(region_id=iso2)
 }
@@ -55,7 +55,7 @@ download_electricity <- function(region_id=NULL, data_source=NULL){
 #'
 #' @examples
 download_thermal_efficiency <- function(region_id=NULL){
-  
+
   params <- list(
     source='NATGAS',
     year=2020,
@@ -65,17 +65,17 @@ download_thermal_efficiency <- function(region_id=NULL){
     product_raw='NATGAS',
     unit='TJ'
   )
-  
+
   # Remove null elements
   params <- purrr::compact(params)
-  
+
   # Create URL params
   url_params <- paste(names(params), params, sep = "=", collapse = "&")
-  
+
   eff <- readr::read_csv(sprintf('https://api.energyandcleanair.org/energy/iea_balance?%s', url_params)) %>%
     mutate(value=value/100) %>%
     select(region_id=iso2, product_raw, flow_raw, unit, year, value)
-  
+
   # Fill missing countries with median
   if(!is.null(region_id)){
     eff <- eff %>%
