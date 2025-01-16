d022d2ac35a46db8b1065b7f203947049b7a5245
Hubert Thieriot <hubert.thieriot@gmail.com>
2025-01-16 13:24:34 +0100

    gas_demand switched to iso2

--- R/check_recent_enough.R
+++ R/check_recent_enough.R
@@ -1,14 +1,14 @@
 check_recent_enough <- function(days=10){
-  
+
   date_from <- lubridate::today() - lubridate::days(days)
-  
-  gas_demand <- download_gas_demand(region_id="EU", date_from=date_from)
+
+  gas_demand <- download_gas_demand(iso2="EU", date_from=date_from)
   if(is.null(gas_demand) || nrow(gas_demand)==0) stop("Missing recent gas demand")
-  
+
   co2 <- download_co2_daily(date_from=date_from, iso2s="EU")
   if(is.null(co2) || nrow(co2)==0) stop("Missing recent CO2")
-  
+
   corrected <- download_corrected_demand(date_from=date_from, region_id="EU")
   if(is.null(corrected) || nrow(corrected)==0) stop("Missing recent corrected gas demand")
-  
+
 }
