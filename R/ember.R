7c58b8548dd0a26f4834e373503c60dd33ce9009
Hubert Thieriot <hubert.thieriot@gmail.com>
2025-02-14 14:47:56 +0100

    -EMBER api only

--- R/ember.R
+++ R/ember.R
@@ -1,10 +1,28 @@
-ember.get_eu_generation <- function(){
-  read_csv('data/ember-eu-generation.csv') %>%
-    filter(country_or_region=='Europe') %>%
-    # Remove double counting
-    filter(variable %in% c(
-      'Bioenergy', 'Coal', 'Gas', 'Hydro', 'Nuclear', 'Other Fossil',
-      'Other Renewables', 'Solar', 'Wind'
-    )) %>%
-    tibble()
+ember.get_eu_generation <- function(frequency='yearly'){
+
+    readRenviron(".Renviron")
+    key <- Sys.getenv("EMBER_KEY")
+
+    base_url <- "https://api.ember-energy.org"
+    query_url <- paste0(
+      base_url, "/v1/electricity-generation/", frequency,
+      "?entity=EU&is_aggregate_series=false&start_date=2000&api_key=", key
+    )
+    response <- httr::GET(query_url)
+    # Read as json from the data argument
+    data <- (httr::content(response, "text", encoding = "UTF-8") %>%
+      jsonlite::fromJSON())$data
+
+    # data %>% distinct(is_aggregate_entity, series)
+
+    data %>% mutate(iso2=entity,
+                    value_mwh=generation_twh*1e6,
+                    source=series
+                    ) %>%
+      select(iso2, source, value_mwh, date) %>%
+      arrange(desc(date)) %>%
+      mutate(date=case_when(
+        frequency=='yearly' ~ paste0(date, "-01-01"),
+        T ~ date
+      ))
 }
