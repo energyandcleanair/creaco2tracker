99ca53fc1e480596b26d98e14ad601a17f659cde
Hubert Thieriot <hubert.thieriot@gmail.com>
2025-02-14 14:48:47 +0100

    -minor

--- R/validation.R
+++ R/validation.R
@@ -7,13 +7,13 @@ validate_power <- function(pwr_demand=download_pwr_demand(), folder="validation"
 
   ember_explorer <- ember.get_eu_generation()
   ember_1 <- ember_explorer %>%
-    mutate(source=recode(variable,
+    mutate(source=recode(source,
                          Gas='Fossil Gas',
                          Bioenergy='Other',
                          `Other Fossil`='Other',
                          `Other Renewables`='Other')) %>%
-    group_by(year, source) %>%
-    summarise(value_twh=sum(generation_twh),
+    group_by(year=as.numeric(date), source) %>%
+    summarise(value_twh=sum(value_mwh)/1e6,
               data_source='EMBER (Data Explorer)')
 
 
@@ -27,7 +27,9 @@ validate_power <- function(pwr_demand=download_pwr_demand(), folder="validation"
   ember_catalogue <- read_csv(filepath)
   ember_2 <- ember_catalogue %>%
     filter(`Area type`=='Country',
-           EU==1, Unit=='TWh', Category=='Electricity generation') %>%
+           EU==1,
+           Unit=='TWh',
+           Category=='Electricity generation') %>%
     filter(Variable %in% c(
       'Coal','Gas','Hydro','Nuclear','Other Fossil', 'Other Renewables',
       'Solar', 'Wind', 'Bioenergy'
@@ -42,8 +44,6 @@ validate_power <- function(pwr_demand=download_pwr_demand(), folder="validation"
     summarise(value_twh = sum(Value),
               data_source='EMBER (Data Catalogue)') %>%
     ungroup()
-
-
 
 
   data <- bind_rows(
@@ -65,7 +65,7 @@ validate_power <- function(pwr_demand=download_pwr_demand(), folder="validation"
               data_source='ENTSOE')
   )
 
-
+  # EU Comparison
   ggplot(data) +
     geom_line(aes(year, value_twh, col=data_source)) +
     facet_wrap(~source) +
@@ -74,5 +74,25 @@ validate_power <- function(pwr_demand=download_pwr_demand(), folder="validation"
          subtitle="TWh",
          x=NULL,
          y=NULL)
+
+
+  # Check why we're missing gas
+  ember_catalogue %>%
+    filter(Variable=='Gas',
+           EU==1,
+           Year==max(Year)
+           ) %>%
+    mutate(iso2=countrycode::countrycode(`Country code`, 'iso3c', 'iso2c'),
+           source='Fossil Gas',
+           year=Year) %>%
+    select(iso2, source, year, value_ember=Value) %>%
+    left_join(pwr_demand %>%
+                group_by(iso2, year=year(date), source) %>%
+                summarise(value_crea=sum(value_mwh, na.rm=T) / 1e6)
+    ) %>%
+    pivot_longer(c(value_ember, value_crea), names_to='data_source', values_to='value_twh') %>%
+    ggplot() +
+    geom_col(aes(iso2, value_twh, fill=data_source),
+             position='dodge')
 
 }
