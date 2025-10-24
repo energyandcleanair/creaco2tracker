e831ccce9da26f7a3f842019e6e9a5c1accc169a
hubert-thieriot <hubert.thieriot@gmail.com>
2025-10-24 09:05:15 +0200

    -adding logs

--- R/iea.R
+++ R/iea.R
@@ -32,7 +32,7 @@ iea.get_balance <- function(year_from = 2000, year_to = 2022, iso2, use_cache = 
 
 iea.get_conversion_factors <- function(year_from = 2000, year_to = 2022, iso2, use_cache = TRUE) {
 
-    # Generate a hash of the parameters
+  # Generate a hash of the parameters
   param_hash <- digest::digest(c(year_from, year_to, iso2))
 
   # Create a filename using the hash
@@ -44,12 +44,14 @@ iea.get_conversion_factors <- function(year_from = 2000, year_to = 2022, iso2, u
     return(readRDS(filepath))
   } else {
     # Fetch the data
+    print(glue("Getting conversion factors from IEA for {iso2} from {year_from} to {year_to}"))
     result <- creahelpers::api.get("api.energyandcleanair.org/energy/iea_conversion",
                                    year_from = year_from,
                                    year_to = year_to,
                                    country = iso2,
                                    api_key = Sys.getenv("API_KEY"))
 
+    print(glue("Got {nrow(result)} records"))
     # Save the fetched data to cache
     if (!dir.exists(cache_dir)) {
       dir.create(cache_dir)
